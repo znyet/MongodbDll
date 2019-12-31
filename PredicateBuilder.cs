0445041d649cd37559f838c55f0fe20e7006ad48
叶涛 <821023977@qq.com>
2019-12-31 12:57:23 +0800

    1

--- PredicateBuilder.cs
+++ PredicateBuilder.cs
@@ -6,7 +6,18 @@ namespace ZLink.Elc.Mogo.Service
 {
     public static class PredicateBuilder
     {
+        /// <summary>
+        /// 机关函数应用True时：单个AND有效，多个AND有效；单个OR无效，多个OR无效；混应时写在AND后的OR有效  
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static Expression<Func<T, bool>> True<T>() { return f => true; }
+
+        /// <summary>
+        /// 机关函数应用False时：单个AND无效，多个AND无效；单个OR有效，多个OR有效；混应时写在OR后面的AND有效  
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static Expression<Func<T, bool>> False<T>() { return f => false; }
 
         public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1,
